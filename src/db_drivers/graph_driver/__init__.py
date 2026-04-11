2e6814f5deaf4b015b2936efe50c40e620487ed4
NMuravya <nmuravya@mail.ru>
2026-04-11 14:38:29 +0300

    fix: make neo4j import optional to avoid crash when package not installed

--- src/db_drivers/graph_driver/__init__.py
+++ src/db_drivers/graph_driver/__init__.py
@@ -1,4 +1,8 @@
-from .connectors.Neo4jConnector import Neo4jConnector, DEFAULT_NEO4J_CONFIG
+try:
+    from .connectors.Neo4jConnector import Neo4jConnector, DEFAULT_NEO4J_CONFIG
+except ImportError:
+    Neo4jConnector = None  # type: ignore[assignment,misc]
+    DEFAULT_NEO4J_CONFIG = None  # type: ignore[assignment]
 from .connectors.InMemoryGraphConnector import InMemoryGraphConnector, DEFAULT_INMEMORYGRAPH_CONFIG
 from .GraphDriver import GraphDriver, GraphDriverConfig
 from .utils import GraphDBConnectionConfig
